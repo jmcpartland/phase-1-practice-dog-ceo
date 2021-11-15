8f49a6de2c34f3bf0e24aa7df8143f4fc925af16
Joe McPartland <joemcpartland@gmail.com>
2021-11-15 16:46:24 -0700

    Challenge 1 complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+const imgContainer = document.querySelector('#dog-image-container');
+
+// function renderOneDog(dog) {
+//     const li = document.createElement('li');
+//     li.className = 'dogs';
+//     li.innerHTML = `<img src="${dog}">`
+//     imgContainer.appendChild(li);
+// };
+
+function renderOneDog(dog) {
+    const dogImg = document.createElement("img");
+    dogImg.src = dog;
+    // dogImg.innerHTML = `<img src="${dog}">`
+    imgContainer.appendChild(dogImg);
+};
+
+
+function getDogImages() {
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(results => {
+        // console.log(results.message);
+        results.message.forEach(dog => renderOneDog(dog))
+      });
+
+};
+
+getDogImages();
